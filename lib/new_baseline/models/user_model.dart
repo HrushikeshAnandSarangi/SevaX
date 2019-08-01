b7104c9ddd16e80265347ce15e8ea97e9933e5a9
ren_ec <renjithecme@gmail.com>
2019-08-01 17:31:50 +0530

    few models

---
+++ lib/new_baseline/models/user_model.dart
@@ -0,0 +1,119 @@
+import 'package:sevaexchange/models/models.dart';
+
+class UserModel extends DataModel {
+  String bio;
+  String email;
+  String fullname;
+  List<String> interests;
+  List<String> membershipTimebanks;
+  List<String> membershipCampaigns;
+  String photoURL;
+  String sevaUserID;
+  List<String> skills;
+  num currentBalance;
+  String timezone;
+
+  UserModel(
+      {this.bio,
+      this.email,
+      this.fullname,
+      this.photoURL,
+      this.interests,
+      this.membershipCampaigns,
+      this.membershipTimebanks,
+      this.sevaUserID,
+      this.skills,
+      this.currentBalance,
+      this.timezone});
+
+  UserModel.fromMap(Map<String, dynamic> map) {
+    if (map.containsKey('bio')) {
+      this.bio = map['bio'];
+    }
+    if (map.containsKey('email')) {
+      this.email = map['email'];
+    }
+    if (map.containsKey('fullname')) {
+      this.fullname = map['fullname'];
+    }
+    if (map.containsKey('photourl')) {
+      this.photoURL = map['photourl'];
+    }
+    if (map.containsKey('interests')) {
+      List<String> interestsList = List.castFrom(map['interests']);
+      this.interests = interestsList;
+    }
+    if (map.containsKey('membership_campaigns')) {
+      List<String> campaignList = List.castFrom(map['membership_campaigns']);
+      this.membershipCampaigns = campaignList;
+    }
+    if (map.containsKey('membership_timebanks')) {
+      List<String> timebanksList = List.castFrom(map['membership_timebanks']);
+      this.membershipTimebanks = timebanksList;
+    }
+    if (map.containsKey('sevauserid')) {
+      this.sevaUserID = map['sevauserid'];
+    }
+    if (map.containsKey('skills')) {
+      List<String> skillsList = List.castFrom(map['skills']);
+      this.skills = skillsList;
+    }
+    if (map.containsKey('currentBalance')) {
+      this.currentBalance = map['currentBalance'];
+    } else {
+      this.currentBalance = 0;
+    }
+    if (map.containsKey('timezone')) {
+      this.timezone = map['timezone'];
+    } else {
+      this.timezone = 'PT';
+    }
+  }
+
+  @override
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> object = {};
+
+    if (this.bio != null && this.bio.isNotEmpty) {
+      object['bio'] = this.bio;
+    }
+    if (this.email != null && this.email.isNotEmpty) {
+      object['email'] = this.email;
+    }
+    if (this.fullname != null && this.fullname.isNotEmpty) {
+      object['fullname'] = this.fullname;
+    }
+    if (this.photoURL != null && this.photoURL.isNotEmpty) {
+      object['photourl'] = this.photoURL;
+    }
+    if (this.interests != null && this.interests.isNotEmpty) {
+      object['interests'] = this.interests;
+    }
+    if (this.membershipCampaigns != null &&
+        this.membershipCampaigns.isNotEmpty) {
+      object['membership_campaigns'] = this.membershipCampaigns;
+    }
+    if (this.membershipTimebanks != null &&
+        this.membershipTimebanks.isNotEmpty) {
+      object['membership_timebanks'] = this.membershipTimebanks;
+    }
+    if (this.sevaUserID != null && this.sevaUserID.isNotEmpty) {
+      object['sevauserid'] = this.sevaUserID;
+    }
+    if (this.skills != null && this.skills.isNotEmpty) {
+      object['skills'] = this.skills;
+    }
+    if (this.currentBalance != null) {
+      object['currentBalance'] = this.currentBalance;
+    } else {
+      object['currentBalance'] = 0;
+    }
+    if (this.timezone != null) {
+      object['timezone'] = this.timezone;
+    } else {
+      object['timezone'] = 'PT';
+    }
+
+    return object;
+  }
+}
