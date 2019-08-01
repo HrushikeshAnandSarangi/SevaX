032f14f2b88f50690e540458ea92b7acfc0bfe52
Ajil Oommen <ajilo297@gmail.com>
2019-08-01 18:14:02 +0530

    Fixed path

--- lib/provider_setup.dart
+++ lib/provider_setup.dart
@@ -1,11 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
-import 'package:sevaexchange/services/authentication/authentication_service.dart';
-import 'package:sevaexchange/services/authentication/email_authentication_service.dart';
-import 'package:sevaexchange/services/authentication/google_authentication_service.dart';
-import 'package:sevaexchange/services/local_storage/local_storage_service.dart';
-
+import 'package:sevaexchange/new_baseline/services/authentication/authentication_service.dart';
+import 'package:sevaexchange/new_baseline/services/authentication/email_authentication_service.dart';
+import 'package:sevaexchange/new_baseline/services/authentication/google_authentication_service.dart';
+import 'package:sevaexchange/new_baseline/services/local_storage/local_storage_service.dart';
 List<SingleChildCloneableWidget> providers = [
   ...independentServices,
   ...dependentServices,
