bdcaf9115940ebeb6d72fbbc6f37163aea8d4fa1
drabu <muslim9999>
2019-11-29 19:50:16 +0530

    shadowing edit feeds

--- lib/views/news/news_card_view.dart
+++ lib/views/news/news_card_view.dart
@@ -36,24 +36,24 @@ class NewsCardView extends StatelessWidget {
           //   icon: Icon(Icons.share),
           //   onPressed: () => _shareNews(context),
           // ),
-
-          newsModel.sevaUserId == SevaCore.of(context).loggedInUser.sevaUserID
-              ? IconButton(
-                  icon: Icon(Icons.edit),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => NewsEdit(
-                          newsModel: newsModel,
-                          timebankId:
-                              SevaCore.of(context).loggedInUser.currentTimebank,
-                        ),
-                      ),
-                    );
-                  },
-                )
-              : Offstage()
+          //shadowing for now as edit feed is not yet completed
+          // newsModel.sevaUserId == SevaCore.of(context).loggedInUser.sevaUserID
+          //     ? IconButton(
+          //         icon: Icon(Icons.edit),
+          //         onPressed: () {
+          //           Navigator.push(
+          //             context,
+          //             MaterialPageRoute(
+          //               builder: (context) => NewsEdit(
+          //                 newsModel: newsModel,
+          //                 timebankId:
+          //                     SevaCore.of(context).loggedInUser.currentTimebank,
+          //               ),
+          //             ),
+          //           );
+          //         },
+          //       )
+          //     : Offstage()
         ],
       ),
       body: SafeArea(
