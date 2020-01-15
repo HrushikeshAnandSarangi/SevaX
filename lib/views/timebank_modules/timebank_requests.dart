0765bcedaa3f5d235f0fd738e6b632720f7b4f64
venkatesh bathina <venkatesh@uipep.com>
2020-01-15 15:14:35 +0530

    Correcting spelling

--- lib/views/timebank_modules/timebank_requests.dart
+++ lib/views/timebank_modules/timebank_requests.dart
@@ -294,7 +294,7 @@ class RequestsState extends State<RequestsModule> {
         // return object of type Dialog
         return AlertDialog(
           title: new Text("Protected Timebank"),
-          content: new Text("You cannot post requests in a protcted timebank"),
+          content: new Text("You cannot post requests in a protected timebank"),
           actions: <Widget>[
             // usually buttons at the bottom of the dialog
             new FlatButton(
