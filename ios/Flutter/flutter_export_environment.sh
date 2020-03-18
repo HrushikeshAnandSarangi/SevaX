3bd6f1d56a029678d90011601a5180e76bae5ce3
venkatesh bathina <venkatesh@uipep.com>
2020-03-18 19:40:10 +0530

    Removing margin from left in applied button

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/venkateshbathina/Documents/flutter/flutterSDK/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/venkateshbathina/Documents/seva-exchange"
+export "FLUTTER_APPLICATION_PATH=/Users/venkateshbathina/Desktop/seva-exchange"
 export "FLUTTER_TARGET=lib/main_app.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
