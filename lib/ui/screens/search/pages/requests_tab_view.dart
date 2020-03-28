ae9ad64915794d6975616117b60db4b3b2727363
shubham <gshubham030@gmail.com>
2020-03-28 19:06:18 +0530

    integrated request page with data from elastic search

--- lib/ui/screens/search/pages/requests_tab_view.dart
+++ lib/ui/screens/search/pages/requests_tab_view.dart
@@ -1,21 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:sevaexchange/models/request_model.dart';
+import 'package:sevaexchange/ui/screens/search/bloc/queries.dart';
+import 'package:sevaexchange/ui/screens/search/bloc/search_bloc.dart';
 import 'package:sevaexchange/ui/screens/timebank/widgets/timebank_request_card.dart';
+import 'package:sevaexchange/utils/bloc_provider.dart';
+import 'package:sevaexchange/views/core.dart';
 
 class RequestsTabView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: 5,
-      itemBuilder: (context, index) {
-        return TimebankRequestCard(
-          title: "Fake Credit Card Generator",
-          subtitle: "Real Money",
-          isApplied: index % 2 == 0,
-          startTime: 165,
-          endTime: 195,
-        );
-      },
+    final _bloc = BlocProvider.of<SearchBloc>(context);
+    return Container(
+      child: StreamBuilder<String>(
+        stream: _bloc.searchText,
+        builder: (context, search) {
+          if (search.data == null || search.data == "") {
+            return Center(child: Text("Search Something"));
+          }
+          return StreamBuilder<List<RequestModel>>(
+            stream: Searches.searchRequests(queryString: search.data),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                shrinkWrap: true,
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  final request = snapshot.data[index];
+                  return TimebankRequestCard(
+                    photoUrl: request.photoUrl,
+                    title: request.title,
+                    subtitle: request.description,
+                    startTime: request.requestStart,
+                    endTime: request.requestEnd,
+                    isApplied: request.acceptors.contains(
+                            SevaCore.of(context).loggedInUser.email) ||
+                        request.approvedUsers
+                            .contains(SevaCore.of(context).loggedInUser.email),
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
