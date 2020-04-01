e11e08f203f3df17bc9224db6aece79d3ae08e58
shubham <gshubham030@gmail.com>
2020-04-01 20:13:17 +0530

    updated bloc to include timebank and user info

--- lib/ui/screens/search/bloc/search_bloc.dart
+++ lib/ui/screens/search/bloc/search_bloc.dart
@@ -1,9 +1,17 @@
 import 'package:rxdart/rxdart.dart';
 import 'package:rxdart/subjects.dart';
+import 'package:sevaexchange/models/user_model.dart';
+import 'package:sevaexchange/new_baseline/models/timebank_model.dart';
 import 'package:sevaexchange/ui/utils/debouncer.dart';
 import 'package:sevaexchange/utils/bloc_provider.dart';
 
 class SearchBloc extends BlocBase {
+  final TimebankModel timebank;
+  final UserModel user;
+  SearchBloc({this.timebank, this.user})
+      : assert(timebank != null),
+        assert(user != null);
+
   final _searchText = BehaviorSubject<String>();
   final _debouncer = Debouncer(milliseconds: 500);
 
