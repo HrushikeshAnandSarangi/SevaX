32fad1b851ceeebc673f57b91db9b9c3d8442f08
uipep_drabu <burhan@uipep.com>
2020-04-07 13:39:02 +0530

    added request mode condition in request flow

--- lib/views/exchange/createrequest.dart
+++ lib/views/exchange/createrequest.dart
@@ -248,6 +248,7 @@ class RequestCreateFormState extends State<RequestCreateForm> {
                         : "Ex: Small carpentry work...",
                   ),
                   keyboardType: TextInputType.text,
+                  initialValue: widget.offer != null ? widget.offer.title : "",
                   textCapitalization: TextCapitalization.sentences,
                   style: textStyle,
                   validator: (value) {
@@ -281,10 +282,9 @@ class RequestCreateFormState extends State<RequestCreateForm> {
                     hintText: 'Your Request \nand any #hashtags',
                     hintStyle: textStyle,
                   ),
-                  initialValue:
-                      widget.isOfferRequest != null && widget.isOfferRequest
-                          ? ""
-                          : "",
+                  initialValue: widget.offer != null && widget.isOfferRequest
+                      ? widget.offer.description
+                      : "",
                   keyboardType: TextInputType.multiline,
                   maxLines: 2,
                   validator: (value) {
