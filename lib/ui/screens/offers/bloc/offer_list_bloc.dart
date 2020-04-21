7467fd0b929e69fcafc0abcfeef127d91d9d3280
shubham <gshubham030@gmail.com>
2020-04-21 19:43:06 +0530

    test bloc and and updated closing of keyboard

--- lib/ui/screens/offers/bloc/offer_list_bloc.dart
+++ lib/ui/screens/offers/bloc/offer_list_bloc.dart
@@ -5,6 +5,10 @@ import 'package:sevaexchange/utils/bloc_provider.dart';
 class OfferListBloc extends BlocBase {
   final _myOffers = BehaviorSubject<List<OfferModel>>();
   final _timebankOffers = BehaviorSubject<List<OfferModel>>();
+
+  Stream<List<OfferModel>> get myOffers => _myOffers.stream;
+  Stream<List<OfferModel>> get timebankOffers => _timebankOffers.stream;
+
   @override
   void dispose() {
     _myOffers.close();
