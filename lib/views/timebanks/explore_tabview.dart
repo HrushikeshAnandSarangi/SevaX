7db8f5aae1afdb4bcbfc7025d80fd82f05519fae
aditya41 <aditya@uipep.com>
2020-04-30 17:16:54 +0530

    ...

--- lib/views/timebanks/explore_tabview.dart
+++ lib/views/timebanks/explore_tabview.dart
@@ -6,6 +6,7 @@ import 'package:sevaexchange/utils/firestore_manager.dart' as FirestoreManager;
 import 'package:sevaexchange/views/core.dart';
 import 'package:sevaexchange/views/onboarding/findcommunitiesview.dart';
 import 'package:sevaexchange/views/timebanks/join_sub_timebank.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ExploreTabView extends StatefulWidget {
   ExploreTabView();
@@ -15,25 +16,26 @@ class ExploreTabView extends StatefulWidget {
 }
 
 class _ExploreTabViewState extends State<ExploreTabView> {
-  CommunityModel communityModel = CommunityModel({});
+//  CommunityModel communityModel = CommunityModel({});
   @override
   void initState() {
     log("exlpore page init");
     // TODO: implement initState
     super.initState();
-    getModelData();
+//    getModelData();
     setState(() {});
   }
 
-  void getModelData() async {
-    Future.delayed(Duration.zero, () {
-      FirestoreManager.getCommunityDetailsByCommunityId(
-              communityId: SevaCore.of(context).loggedInUser.currentCommunity)
-          .then((onValue) {
-        communityModel = onValue;
-      });
-    });
-  }
+//  void getModelData() async {
+//    Future.delayed(Duration.zero, () {
+//      FirestoreManager.getCommunityDetailsByCommunityId(
+//          communityId: SevaCore.of(context).loggedInUser.currentCommunity)
+//          .then((onValue) {
+//        communityModel = onValue;
+//        print("widget.communityPrimaryTimebankId is --- ${communityModel.id} --- ${communityModel.primary_timebank}");
+//      });
+//    });
+//  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,44 +53,82 @@ class _ExploreTabViewState extends State<ExploreTabView> {
             style: TextStyle(fontSize: 18, fontFamily: 'Europa'),
           ),
         ),
-        body: Column(
-          children: <Widget>[
-            TabBar(
-              labelColor: Theme.of(context).primaryColor,
-              indicatorColor: Theme.of(context).primaryColor,
-              indicatorSize: TabBarIndicatorSize.label,
-              unselectedLabelColor: Colors.black,
-              isScrollable: true,
-              tabs: [
-                Tab(
-                  text: "Find Timebanks",
+        body: FutureBuilder<CommunityModel>(
+          future: FirestoreManager.getCommunityDetailsByCommunityId(
+              communityId: SevaCore.of(context).loggedInUser.currentCommunity),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) return Text(snapshot.error.toString());
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return shimmerWidget;
+            }
+            CommunityModel communityModel = snapshot.data;
+            return Column(
+              children: <Widget>[
+                TabBar(
+                  labelColor: Theme.of(context).primaryColor,
+                  indicatorColor: Theme.of(context).primaryColor,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  unselectedLabelColor: Colors.black,
+                  isScrollable: true,
+                  tabs: [
+                    Tab(
+                      text: "Find Timebanks",
+                    ),
+                    Tab(
+                      text: "Groups within ${snapshot.data.name ?? "Timebank"}  ",
+                    ),
+                  ],
                 ),
-                Tab(
-                  text: "Groups within ${communityModel.name ?? "Timebank"}",
-                ),
+                Expanded(
+                  child: TabBarView(
+                    children: <Widget>[
+                      FindCommunitiesView(
+                        keepOnBackPress: true,
+                        loggedInUser: SevaCore.of(context).loggedInUser,
+                        showBackBtn: true,
+                        isFromHome: true,
+                      ),
+                      JoinSubTimeBankView(
+                        isFromDash: true,
+                        loggedInUserModel: SevaCore.of(context).loggedInUser,
+                        communityId: communityModel.id,
+                        communityPrimaryTimebankId: communityModel.primary_timebank,
+                      ),
+                    ],
+                  ),
+                )
               ],
-            ),
-            Expanded(
-              child: TabBarView(
-                children: <Widget>[
-                  FindCommunitiesView(
-                    keepOnBackPress: true,
-                    loggedInUser: SevaCore.of(context).loggedInUser,
-                    showBackBtn: true,
-                    isFromHome: true,
-                  ),
-                  JoinSubTimeBankView(
-                    isFromDash: true,
-                    loggedInUserModel: SevaCore.of(context).loggedInUser,
-                    communityId: communityModel.id,
-                    communityPrimaryTimebankId: communityModel.primary_timebank,
-                  ),
-                ],
-              ),
-            )
-          ],
+            );
+          }
         ),
       ),
+    );
+  }
+  Widget get shimmerWidget {
+    return Shimmer.fromColors(
+      child: Container(
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          color: Colors.grey.withAlpha(40),
+        ),
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+        child: ListTile(
+            title: Container(
+              color: Colors.grey.withAlpha(90),
+              height: 10,
+            ),
+            leading: CircleAvatar(
+              backgroundColor: Colors.grey.withAlpha(90),
+            ),
+            subtitle: Container(
+              color: Colors.grey.withAlpha(90),
+              height: 8,
+            )),
+      ),
+      baseColor: Colors.grey,
+      highlightColor: Colors.white,
     );
   }
 }
