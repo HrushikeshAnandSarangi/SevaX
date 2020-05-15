24668a6d9c2589b95510ed8a5eb7df44fce6c6c4
shubham <gshubham030@gmail.com>
2020-05-15 20:07:45 +0530

    created new widget for image picker

---
+++ lib/widgets/image_picker_widget.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:sevaexchange/components/newsimage/image_picker_handler.dart';
+
+///Pass a [Widget] without any gestures or click events
+class ImagePickerWidget extends StatefulWidget {
+  final Widget child;
+  final ValueChanged<File> onChanged;
+  final bool isAspectRatioFixed;
+
+  const ImagePickerWidget(
+      {Key key, this.onChanged, this.child, this.isAspectRatioFixed = true})
+      : assert(child != null),
+        assert(onChanged != null),
+        super(key: key);
+
+  @override
+  _ImagePickerWidgetState createState() => _ImagePickerWidgetState();
+}
+
+class _ImagePickerWidgetState extends State<ImagePickerWidget>
+    with TickerProviderStateMixin, ImagePickerListener {
+  ImagePickerHandler imagePicker;
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    imagePicker = ImagePickerHandler(
+      this,
+      _controller,
+      isAspectRatioFixed: widget.isAspectRatioFixed,
+    );
+    imagePicker.init();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => imagePicker.showDialog(context),
+      child: widget.child,
+    );
+  }
+
+  @override
+  userImage(File _image) {
+    widget.onChanged(_image);
+  }
+}
