1c2018d1598e0daf81bee8ce031bb48bc9e20210
shubham <gshubham030@gmail.com>
2020-05-20 18:40:51 +0530

    chips added to show group names

--- lib/ui/screens/reported_members/pages/reported_member_info.dart
+++ lib/ui/screens/reported_members/pages/reported_member_info.dart
@@ -1,32 +1,46 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:sevaexchange/models/reported_members_model.dart';
 import 'package:sevaexchange/ui/screens/reported_members/widgets/report_info_card.dart';
+import 'package:sevaexchange/ui/screens/reported_members/widgets/reported_member_chip.dart';
 
 enum ACTIONS { REMOVE, MESSAGE }
 
 class ReportedMemberInfo extends StatelessWidget {
   final ReportedMembersModel model;
   final bool isFromTimebank;
+  final VoidCallback removeMember;
+  final VoidCallback messageMember;
 
-  const ReportedMemberInfo({Key key, this.model, this.isFromTimebank})
+  const ReportedMemberInfo(
+      {Key key,
+      this.model,
+      this.isFromTimebank,
+      this.removeMember,
+      this.messageMember})
       : assert(isFromTimebank != null),
         assert(model != null),
         super(key: key);
 
-  static Route<dynamic> route(
-      {ReportedMembersModel model, bool isFromTimebank}) {
+  static Route<dynamic> route({
+    ReportedMembersModel model,
+    bool isFromTimebank,
+    VoidCallback removeMember,
+    VoidCallback messageMember,
+  }) {
     return MaterialPageRoute(
       builder: (BuildContext context) => ReportedMemberInfo(
-        model: model,
-        isFromTimebank: isFromTimebank,
-      ),
+          model: model,
+          isFromTimebank: isFromTimebank,
+          removeMember: removeMember,
+          messageMember: messageMember),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    Map<String, dynamic> reportCountsMap = countReports(model);
+    List<String> keys = List.from(reportCountsMap.keys);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -37,7 +51,12 @@ class ReportedMemberInfo extends StatelessWidget {
         actions: <Widget>[
           PopupMenuButton(
             onSelected: (value) {
-              log(value.toString());
+              if (value == ACTIONS.MESSAGE) {
+                messageMember();
+              } else {
+                removeMember();
+                Navigator.of(context).pop();
+              }
             },
             itemBuilder: (context) => <PopupMenuItem>[
               PopupMenuItem(
@@ -52,33 +71,66 @@ class ReportedMemberInfo extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        child: ListView.separated(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          itemCount: model.reports.length,
-          itemBuilder: (context, index) {
-            Report report = model.reports[index];
-            return (isFromTimebank
-                    ? true
-                    : report.isTimebankReport == isFromTimebank)
-                ? ReportInfoCard(
-                    report: model.reports[index],
-                    isFromTimebank: isFromTimebank,
-                  )
-                : Container();
-          },
-          separatorBuilder: (_, index) {
-            Report report = model.reports[index];
-            return (isFromTimebank
-                    ? true
-                    : report.isTimebankReport == isFromTimebank)
-                ? Divider(
-                    thickness: 1,
-                  )
-                : Container();
-          },
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 10),
+            SizedBox(
+              height: 35,
+              child: ListView.builder(
+                padding: EdgeInsets.only(left: 20),
+                scrollDirection: Axis.horizontal,
+                itemCount: reportCountsMap.length,
+                itemBuilder: (_, index) {
+                  return ReportedMemberChip(
+                    title: keys[index],
+                    count: reportCountsMap[keys[index]],
+                  );
+                },
+              ),
+            ),
+            ListView.separated(
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              itemCount: model.reports.length,
+              itemBuilder: (context, index) {
+                Report report = model.reports[index];
+                return (isFromTimebank
+                        ? true
+                        : report.isTimebankReport == isFromTimebank)
+                    ? ReportInfoCard(
+                        report: report,
+                        isFromTimebank: isFromTimebank,
+                      )
+                    : Container();
+              },
+              separatorBuilder: (_, index) {
+                Report report = model.reports[index];
+                return (isFromTimebank
+                        ? true
+                        : report.isTimebankReport == isFromTimebank)
+                    ? Divider(
+                        thickness: 1,
+                      )
+                    : Container();
+              },
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+Map<String, int> countReports(ReportedMembersModel model) {
+  Map<String, int> map = {};
+  model.reports.forEach((Report report) {
+    if (map.containsKey(report.entityName)) {
+      map[report.isTimebankReport ? "Timebank" : report.entityName] += 1;
+    } else {
+      map[report.isTimebankReport ? "Timebank" : report.entityName] = 1;
+    }
+  });
+  return map;
 }
