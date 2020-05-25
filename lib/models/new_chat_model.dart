aa6ca43662c75ac89cc9ac2505f0062f709a6e6d
shubham <gshubham030@gmail.com>
2020-05-25 17:57:56 +0530

    chat model updated

--- lib/models/new_chat_model.dart
+++ lib/models/new_chat_model.dart
@@ -1,8 +1,10 @@
 class ChatModel {
   List<String> participants;
+  List<ParticipantInfo> participantInfo;
   String lastMessage;
   Map<String, dynamic> unreadStatus;
   List<String> softDeletedBy;
+  Map<dynamic, dynamic> deletedBy;
   bool isTimebankMessage;
   String timebankId;
   String communityId;
@@ -10,9 +12,11 @@ class ChatModel {
 
   ChatModel({
     this.participants,
+    this.participantInfo,
     this.lastMessage,
     this.unreadStatus,
     this.softDeletedBy,
+    this.deletedBy,
     this.isTimebankMessage = false,
     this.timebankId,
     this.communityId,
@@ -21,9 +25,13 @@ class ChatModel {
 
   factory ChatModel.fromMap(Map<String, dynamic> map) => ChatModel(
         participants: List<String>.from(map["participants"].map((x) => x)),
+        participantInfo: List<ParticipantInfo>.from(map["participantInfo"]
+            .map((x) => ParticipantInfo.fromMap(Map<String, dynamic>.from(x)))),
         lastMessage: map["lastMessage"],
         unreadStatus: map["unreadStatus"],
-        softDeletedBy: List<String>.from(map["softDeletedBy"].map((x) => x)),
+        softDeletedBy: map["softDeletedBy"] == null
+            ? []
+            : List<String>.from(map["softDeletedBy"].map((x) => x)),
         isTimebankMessage: map["isTimebankMessage"],
         timebankId: map["timebankId"],
         communityId: map["communityId"],
@@ -32,12 +40,54 @@ class ChatModel {
 
   Map<String, dynamic> toMap() => {
         "participants": List<dynamic>.from(participants.map((x) => x)),
+        "participantInfo":
+            List<dynamic>.from(participantInfo.map((x) => x.toMap())),
         "lastMessage": lastMessage,
         "unreadStatus": unreadStatus,
-        "softDeletedBy": List<dynamic>.from(softDeletedBy.map((x) => x)),
+        "softDeletedBy": softDeletedBy,
         "isTimebankMessage": isTimebankMessage,
         "timebankId": timebankId,
         "communityId": communityId,
         "timestamp": timestamp,
       };
 }
+
+class ParticipantInfo {
+  String id;
+  String name;
+  String photoUrl;
+  MessageType type;
+
+  ParticipantInfo({
+    this.id,
+    this.name,
+    this.photoUrl,
+    this.type,
+  });
+
+  factory ParticipantInfo.fromMap(Map<String, dynamic> map) => ParticipantInfo(
+        id: map["id"],
+        name: map["name"],
+        photoUrl: map["photoUrl"],
+        type: typeMapper[map["type"]],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+        "photoUrl": photoUrl,
+        "type": type.toString().split('.')[1],
+      };
+}
+
+enum MessageType {
+  TYPE_PERSONAL,
+  TYPE_TIMEBANK,
+  TYPE_GROUP,
+}
+
+Map<String, MessageType> typeMapper = {
+  "TYPE_PERSONAL": MessageType.TYPE_PERSONAL,
+  "TYPE_TIMEBANK": MessageType.TYPE_TIMEBANK,
+  "TYPE_GROUP": MessageType.TYPE_GROUP,
+};
