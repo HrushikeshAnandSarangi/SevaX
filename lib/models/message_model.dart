a6c7504c5660d3ba2313569b5868b6ff976ee698
shubham <gshubham030@gmail.com>
2020-05-27 18:18:23 +0530

    message model updated

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -1,66 +1,111 @@
-import 'package:sevaexchange/models/data_model.dart';
-
-class MessageModel extends DataModel {
+class MessageModel {
   String message;
   String fromId;
   String toId;
+  MessageType type;
   int timestamp;
-  bool isRead;
 
   MessageModel({
     this.message,
     this.fromId,
     this.toId,
+    this.type,
     this.timestamp,
-    this.isRead = false,
   });
 
-  MessageModel.fromMap(Map<String, dynamic> map) {
-    if (map.containsKey('message')) {
-      this.message = map['message'];
-    }
+  factory MessageModel.fromMap(Map<String, dynamic> map) => MessageModel(
+        message: map["message"],
+        fromId: map["fromId"],
+        toId: map["toId"],
+        type: map.containsKey('type')
+            ? _typeMapper[map["type"]]
+            : MessageType.MESSAGE,
+        timestamp: map["timestamp"],
+      );
 
-    if (map.containsKey('fromId')) {
-      this.fromId = map['fromId'];
-    }
-
-    if (map.containsKey('toId')) {
-      this.toId = map['toId'];
-    }
-    if (map.containsKey('timestamp')) {
-      this.timestamp = map['timestamp'];
-
-      if (map.containsKey('isRead')) {
-        this.isRead = map['isRead'];
-      }
-    }
-  }
-
-  @override
-  Map<String, dynamic> toMap() {
-    Map<String, dynamic> map = {};
-
-    if (this.message != null) {
-      map['message'] = this.message;
-    }
-
-    if (this.fromId != null) {
-      map['fromId'] = this.fromId;
-    }
-
-    if (this.toId != null) {
-      map['toId'] = this.toId;
-    }
-
-    if (this.timestamp != null) {
-      map['timestamp'] = this.timestamp;
-    }
-
-    if (this.isRead != null) {
-      map['isRead'] = this.isRead;
-    }
-
-
-    return map;
-  }
+  Map<String, dynamic> toMap() => {
+        "message": message,
+        "fromId": fromId,
+        "toId": toId,
+        "type": type != null ? type.toString().split('.')[1] : "MESSAGE",
+        "timestamp": timestamp,
+      };
 }
+
+// class MessageModel extends DataModel {
+//   String message;
+//   String fromId;
+//   String toId;
+//   int timestamp;
+//   bool isRead;
+
+//   MessageModel({
+//     this.message,
+//     this.fromId,
+//     this.toId,
+//     this.timestamp,
+//     this.isRead = false,
+//   });
+
+//   MessageModel.fromMap(Map<String, dynamic> map) {
+//     if (map.containsKey('message')) {
+//       this.message = map['message'];
+//     }
+
+//     if (map.containsKey('fromId')) {
+//       this.fromId = map['fromId'];
+//     }
+
+//     if (map.containsKey('toId')) {
+//       this.toId = map['toId'];
+//     }
+//     if (map.containsKey('timestamp')) {
+//       this.timestamp = map['timestamp'];
+
+//       if (map.containsKey('isRead')) {
+//         this.isRead = map['isRead'];
+//       }
+//     }
+//   }
+
+//   @override
+//   Map<String, dynamic> toMap() {
+//     Map<String, dynamic> map = {};
+
+//     if (this.message != null) {
+//       map['message'] = this.message;
+//     }
+
+//     if (this.fromId != null) {
+//       map['fromId'] = this.fromId;
+//     }
+
+//     if (this.toId != null) {
+//       map['toId'] = this.toId;
+//     }
+
+//     if (this.timestamp != null) {
+//       map['timestamp'] = this.timestamp;
+//     }
+
+//     if (this.isRead != null) {
+//       map['isRead'] = this.isRead;
+//     }
+
+//     return map;
+//   }
+// }
+
+enum MessageType {
+  FEED,
+  MESSAGE,
+  TIMBANK,
+  IMAGE,
+}
+
+Map<String, MessageType> _typeMapper = {
+  "FEED": MessageType.FEED,
+  "MESSAGE": MessageType.MESSAGE,
+  "TIMBANK": MessageType.TIMBANK,
+  "IMAGE": MessageType.IMAGE,
+};
