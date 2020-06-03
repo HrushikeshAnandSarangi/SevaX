f7f24b6c41fab83503e21a37bca9251a58707a45
shubham <gshubham030@gmail.com>
2020-06-03 20:21:51 +0530

    select timebank and select volunteer localization fixed

--- lib/utils/members_of_timebank.dart
+++ lib/utils/members_of_timebank.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:sevaexchange/constants/sevatitles.dart';
+import 'package:sevaexchange/internationalization/app_localization.dart';
 import 'package:sevaexchange/models/chat_model.dart' as prefix;
 import 'package:sevaexchange/models/news_model.dart';
 import 'package:sevaexchange/models/user_model.dart';
@@ -134,7 +135,8 @@ class _SelectMembersInGroupState extends State<SelectMembersFromTimebank> {
     var finalWidget = Scaffold(
       appBar: AppBar(
         title: Text(
-          "Select volunteer",
+          AppLocalizations.of(context)
+              .translate('select_member', 'select_volunteer'),
           style: TextStyle(fontSize: 18),
         ),
         elevation: 0,
