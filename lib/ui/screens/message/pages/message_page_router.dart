b1c1c38cae05f6b3cb2f83bdcce93ff55b713421
drabu <muslim9999>
2020-06-05 00:28:59 +0530

    updated location of new message

--- lib/ui/screens/message/pages/message_page_router.dart
+++ lib/ui/screens/message/pages/message_page_router.dart
@@ -29,6 +29,41 @@ class _MessagePageRouterState extends State<MessagePageRouter> {
           "Messages",
           style: TextStyle(fontSize: 18),
         ),
+        actions: <Widget>[
+          IconButton(
+            color: Colors.white,
+            icon: Image.asset(
+              createMessageIcon,
+              width: 20,
+              height: 20,
+            ),
+            onPressed: () {
+              if (SevaCore.of(context).loggedInUser.associatedWithTimebanks >
+                  1) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SelectTimeBankForNewChat(),
+                  ),
+                );
+              } else {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SelectMembersFromTimebank(
+                      timebankId:
+                          SevaCore.of(context).loggedInUser.currentTimebank,
+                      newsModel: NewsModel(),
+                      isFromShare: false,
+                      selectionMode: MEMBER_SELECTION_MODE.NEW_CHAT,
+                      userSelected: HashMap(),
+                    ),
+                  ),
+                );
+              }
+            },
+          ),
+        ],
         centerTitle: true,
       ),
       body: Padding(
@@ -38,38 +73,9 @@ class _MessagePageRouterState extends State<MessagePageRouter> {
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
-                IconButton(
-                  color: Theme.of(context).primaryColor,
-                  icon: Image.asset(createMessageIcon, width: 20, height: 20),
-                  onPressed: () {
-                    if (SevaCore.of(context)
-                            .loggedInUser
-                            .associatedWithTimebanks >
-                        1) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SelectTimeBankForNewChat(),
-                        ),
-                      );
-                    } else {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SelectMembersFromTimebank(
-                            timebankId: SevaCore.of(context)
-                                .loggedInUser
-                                .currentTimebank,
-                            newsModel: NewsModel(),
-                            isFromShare: false,
-                            selectionMode: MEMBER_SELECTION_MODE.NEW_CHAT,
-                            userSelected: HashMap(),
-                          ),
-                        ),
-                      );
-                    }
-                  },
-                ),
+                Container(
+                  height: 10,
+                )
               ],
             ),
             StreamBuilder<List<AdminMessageWrapperModel>>(
