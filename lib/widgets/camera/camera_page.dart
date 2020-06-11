7a5ccfa9c35fe49559a85310b0a2f97645f7f675
shubham <gshubham030@gmail.com>
2020-06-11 21:01:51 +0530

    camera feature added for message

---
+++ lib/widgets/camera/camera_page.dart
@@ -0,0 +1,218 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sevaexchange/models/image_caption_model.dart';
+import 'package:sevaexchange/widgets/camera/selected_image_preview.dart';
+
+class CameraPage extends StatefulWidget {
+  final List<CameraDescription> cameras;
+
+  const CameraPage({Key key, this.cameras}) : super(key: key);
+
+  @override
+  _CameraState createState() => _CameraState();
+}
+
+class _CameraState extends State<CameraPage> {
+  CameraController controller;
+  int _cameraIndex = 0;
+  bool _cameraNotAvailable = false;
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void _showInSnackBar(String message) {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  void _initCamera(int index) async {
+    if (controller != null) {
+      await controller.dispose();
+    }
+    controller = CameraController(widget.cameras[index], ResolutionPreset.high);
+
+    // If the controller is updated then update the UI.
+    controller.addListener(() {
+      if (mounted) setState(() {});
+      if (controller.value.hasError) {
+        _showInSnackBar('Camera error ${controller.value.errorDescription}');
+      }
+    });
+
+    try {
+      await controller.initialize();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+    }
+
+    if (mounted) {
+      setState(() {
+        _cameraIndex = index;
+      });
+    }
+  }
+
+  void _onSwitchCamera() {
+    if (controller == null ||
+        !controller.value.isInitialized ||
+        controller.value.isTakingPicture) {
+      return;
+    }
+    final newIndex =
+        _cameraIndex + 1 == widget.cameras.length ? 0 : _cameraIndex + 1;
+    _initCamera(newIndex);
+  }
+
+  void _onTakePictureButtonPress() {
+    _takePicture().then((filePath) {
+      if (filePath != null) {
+        navigateToImagePreview(File(filePath));
+      }
+    });
+  }
+
+  void navigateToImagePreview(File file) {
+    Navigator.push<ImageCaptionModel>(context, MaterialPageRoute(
+      builder: (context) {
+        return SelectedImagePreview(file: file);
+      },
+    )).then((ImageCaptionModel imageCaptionModel) {
+      Navigator.of(context).pop(imageCaptionModel);
+    });
+  }
+
+  Future<void> _onGalleryButtonPress() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    if (pickedFile?.path != null) {
+      navigateToImagePreview(File(pickedFile.path));
+    }
+  }
+
+  String _timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
+
+  Future<String> _takePicture() async {
+    if (!controller.value.isInitialized || controller.value.isTakingPicture) {
+      return null;
+    }
+    final Directory extDir = await getApplicationDocumentsDirectory();
+    final String dirPath = '${extDir.path}/Pictures/sevax';
+    await Directory(dirPath).create(recursive: true);
+    final String filePath = '$dirPath/${_timestamp()}.jpg';
+
+    try {
+      await controller.takePicture(filePath);
+    } on CameraException catch (e) {
+      _showCameraException(e);
+      return null;
+    }
+    return filePath;
+  }
+
+  void _showCameraException(CameraException e) {
+    _showInSnackBar('Error: ${e.code}\n${e.description}');
+  }
+
+  Widget _buildControlBar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        IconButton(
+          color: Colors.white,
+          icon: Icon(Icons.insert_photo),
+          onPressed: _onGalleryButtonPress,
+        ),
+        GestureDetector(
+          onTap: _onTakePictureButtonPress,
+          child: Container(
+            height: 80.0,
+            width: 80.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.white,
+                width: 5.0,
+              ),
+            ),
+          ),
+        ),
+        IconButton(
+          color: Colors.white,
+          icon: Icon(Icons.switch_camera),
+          onPressed: _onSwitchCamera,
+        ),
+      ],
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.cameras == null || widget.cameras.isEmpty) {
+      setState(() {
+        _cameraNotAvailable = true;
+      });
+    }
+    _initCamera(_cameraIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_cameraNotAvailable) {
+      return Center(
+        child: Text('Camera not available /_\\'),
+      );
+    }
+
+    final stack = Stack(
+      children: <Widget>[
+        Container(
+          color: Colors.black,
+          child: Center(
+            child: controller.value.isInitialized
+                ? AspectRatio(
+                    aspectRatio: controller.value.aspectRatio,
+                    child: CameraPreview(controller),
+                  )
+                : Text('Loading camera...'),
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            _buildControlBar(),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(
+                'Tap for photo',
+                style: Theme.of(context)
+                    .textTheme
+                    .subtitle1
+                    .copyWith(color: Colors.white),
+              ),
+            )
+          ],
+        )
+      ],
+    );
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: stack,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (controller != null) {
+      controller.dispose();
+    }
+  }
+}
---
+++ lib/widgets/camera/selected_image_preview.dart
@@ -0,0 +1,91 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:sevaexchange/models/image_caption_model.dart';
+import 'package:sevaexchange/ui/screens/message/widgets/message_input.dart';
+
+class SelectedImagePreview extends StatefulWidget {
+  final File file;
+
+  const SelectedImagePreview({Key key, this.file}) : super(key: key);
+
+  @override
+  _SelectedImagePreviewState createState() => _SelectedImagePreviewState();
+}
+
+class _SelectedImagePreviewState extends State<SelectedImagePreview> {
+  File _file;
+  TextEditingController textController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _file = widget.file;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.crop_rotate),
+            onPressed: () {
+              ImageCropper.cropImage(sourcePath: _file.path).then((File file) {
+                if (file != null) {
+                  setState(() {
+                    _file = file;
+                  });
+                }
+              });
+            },
+          ),
+          // IconButton(
+          //   icon: Icon(Icons.insert_emoticon),
+          //   onPressed: () {},
+          // ),
+          // IconButton(
+          //   icon: Icon(Icons.text_fields),
+          //   onPressed: () {},
+          // ),
+          // IconButton(
+          //   icon: Icon(Icons.edit),
+          //   onPressed: () {},
+          // ),
+        ],
+      ),
+      body: Container(
+        color: Colors.black,
+        child: Stack(
+          children: <Widget>[
+            Center(
+              child: Image.file(_file),
+            ),
+            Positioned(
+              bottom: 12,
+              left: 12,
+              right: 12,
+              child: MessageInput(
+                textController: textController,
+                handleSubmitted: (String value) {
+                  send(_file, value);
+                },
+                hideCameraIcon: true,
+                hintText: "Add a caption",
+                onSend: () {
+                  send(_file, textController.text);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void send(File file, String caption) {
+    ImageCaptionModel model = ImageCaptionModel(file, caption);
+    Navigator.of(context).pop<ImageCaptionModel>(model);
+  }
+}
