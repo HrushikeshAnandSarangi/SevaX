a7aa860b017b149d4b5e9e51752e3dd2af58115d
shubham <gshubham030@gmail.com>
2020-08-04 12:06:09 +0530

    arbify-added

---
+++ lib/l10n/l10n.dart
@@ -0,0 +1,52 @@
+// File generated with arbify_flutter.
+// DO NOT MODIFY BY HAND.
+// ignore_for_file: lines_longer_than_80_chars, non_constant_identifier_names
+// ignore_for_file: unnecessary_brace_in_string_interps
+
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+import 'messages_all.dart';
+
+class S {
+  final String localeName;
+
+  const S(this.localeName);
+
+  static const delegate = ArbifyLocalizationsDelegate();
+
+  static Future<S> load(Locale locale) {
+    final localeName = Intl.canonicalizedLocale(locale.toString());
+
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      return S(localeName);
+    });
+  }
+
+  static S of(BuildContext context) => Localizations.of<S>(context, S);
+
+  String get timebank => Intl.message(
+        'Timebank',
+        name: 'timebank',        
+        desc: 'Timebank label',
+      );
+}
+
+class ArbifyLocalizationsDelegate extends LocalizationsDelegate<S> {
+  const ArbifyLocalizationsDelegate();
+
+  List<Locale> get supportedLocales => [
+        Locale.fromSubtags(languageCode: 'en'),
+  ];
+
+  @override
+  bool isSupported(Locale locale) => [
+        'en',
+      ].contains(locale.languageCode);
+
+  @override
+  Future<S> load(Locale locale) => S.load(locale);
+
+  @override
+  bool shouldReload(ArbifyLocalizationsDelegate old) => false;
+}
