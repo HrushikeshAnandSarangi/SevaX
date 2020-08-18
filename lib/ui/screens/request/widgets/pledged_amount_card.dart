ad6be1d7794937582e4c8237b97cceb8d905efa3
shubham <gshubham030@gmail.com>
2020-08-18 19:05:14 +0530

    ui completed

---
+++ lib/ui/screens/request/widgets/pledged_amount_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class PledgedAmountCard extends StatelessWidget {
+  final String name;
+  final String currency;
+  final String amount;
+  const PledgedAmountCard({
+    Key key,
+    this.name,
+    this.currency,
+    this.amount,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 150,
+          width: double.infinity,
+          padding: EdgeInsets.only(top: 25),
+          child: Card(
+            elevation: 5.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  '$name pledged to donate',
+                  style: TextStyle(color: Colors.grey, fontSize: 16),
+                ),
+                Text(
+                  '$currency$amount',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: CircleAvatar(
+            radius: 30,
+            child: Icon(Icons.check, size: 30),
+            backgroundColor: Theme.of(context).primaryColor,
+            foregroundColor: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+}
