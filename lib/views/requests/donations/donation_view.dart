7754edcf3e5c5e0c370e420ee78e9855eaa28b45
shubham <gshubham030@gmail.com>
2020-08-25 22:20:02 +0530

    /\.

--- lib/views/requests/donations/donation_view.dart
+++ lib/views/requests/donations/donation_view.dart
@@ -370,6 +370,7 @@ class _DonationViewState extends State<DonationView> {
                       if (value) {
                         hideProgress();
                         getSuccessDialog().then(
+                          //to pop the screen
                           (_) => Navigator.of(context).pop(),
                         );
                       }
