2c8a4901dddb2b870fa42a90d8ac44e9e2a57440
Abhishek <abhishekkumargupta11@gmail.com>
2020-10-05 11:58:48 +0530

    removed_donated_and_items_donated_from_profileviewer

---
+++ lib/ui/screens/intro_slider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:intro_slider/intro_slider.dart';
+
+class Into extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IntroSlider(
+      data: [
+        'images/intro_screens/Broadcasting_feeds.png',
+        'images/intro_screens/Groups_and_Projects.png',
+        'images/intro_screens/Messaging_and_communication.png',
+        'images/intro_screens/Requests_and_Offers.png',
+        'images/intro_screens/What_is_a_timebank_and_how_can_people_find_and_join_one.png',
+      ],
+      onSkip: () {},
+    );
+  }
+}
---
+++ packages/intro_slider/lib/src/intro_slider.dart
@@ -0,0 +1,127 @@
+library intro_slider;
+
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class IntroSlider extends StatefulWidget {
+  // final List<Widget> data;
+  final List<String> data;
+  final VoidCallback onSkip;
+  IntroSlider({@required this.data, @required this.onSkip});
+  @override
+  _IntroSliderState createState() => _IntroSliderState();
+}
+
+class _IntroSliderState extends State<IntroSlider> {
+  Timer _timer;
+  final PageController _controller = PageController(initialPage: 0);
+  final _pageIndicator = StreamController<int>.broadcast();
+
+  @override
+  void initState() {
+    if (widget.data.length > 1) {
+      WidgetsBinding.instance.addPostFrameCallback(
+        (timeStamp) {
+          _timer = Timer.periodic(
+            Duration(seconds: 2),
+            (timer) async {
+              await _controller.nextPage(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+              );
+              _pageIndicator.add(
+                _controller.page.toInt() % widget.data.length,
+              );
+            },
+          );
+        },
+      );
+    }
+    super.initState();
+  }
+
+  // dispose method
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  // slider
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView.builder(
+            controller: _controller,
+            itemBuilder: (context, index) {
+              return Image.asset(
+                widget.data[index % widget.data.length],
+                fit: BoxFit.cover,
+              );
+              // return widget.data[index % widget.data.length];
+            },
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, bottom: 8),
+              child: Row(
+                children: [
+                  StreamBuilder<int>(
+                    stream: _pageIndicator.stream,
+                    builder: (context, snapshot) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(
+                          widget.data.length,
+                          (index) => Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Container(
+                              height: index == (snapshot.data ?? 0) ? 15 : 8,
+                              width: index == (snapshot.data ?? 0) ? 15 : 8,
+                              decoration: BoxDecoration(
+                                color: index == (snapshot.data ?? 0)
+                                    ? Colors.white
+                                    : Colors.white38,
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  Spacer(),
+                  FlatButton(
+                    color: Colors.transparent,
+                    onPressed: widget.onSkip,
+                    child: Row(
+                      children: [
+                        Text(
+                          'Skip',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
