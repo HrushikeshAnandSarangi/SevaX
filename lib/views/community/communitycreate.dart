562a2c597e0dee817f6ca0a7a0f3aac49eba6628
aditya41 <aditya@uipep.com>
2021-01-07 00:27:59 +0530

    currentTimebank was not getting updated while creating a community

--- lib/views/community/communitycreate.dart
+++ lib/views/community/communitycreate.dart
@@ -925,14 +925,18 @@ class CreateEditCommunityViewFormState
                                           'communities': FieldValue.arrayUnion(
                                               [snapshot.data.community.id]),
                                           'currentCommunity':
-                                              snapshot.data.community.id
+                                              snapshot.data.community.id,
+                                          'currentTimebank':
+                                          snapshot.data.community.primary_timebank,
                                         });
 
                                         setState(() {
                                           SevaCore.of(context)
-                                                  .loggedInUser
-                                                  .currentCommunity =
+                                                  .loggedInUser.currentCommunity =
                                               snapshot.data.community.id;
+                                          SevaCore.of(context)
+                                              .loggedInUser.currentCommunity =
+                                              snapshot.data.community.primary_timebank;
                                         });
 
                                         globals.timebankAvatarURL = null;
