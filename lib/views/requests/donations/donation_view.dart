77e848dc8cac4870ed96f9b44dec6e08a88d30a8
aditya41 <aditya@uipep.com>
2021-01-15 19:42:17 +0530

    small fixes in donation

--- lib/views/requests/donations/donation_view.dart
+++ lib/views/requests/donations/donation_view.dart
@@ -1237,7 +1237,7 @@ class _DonationViewState extends State<DonationView> {
     //   ),
     //   message: message,
     // );
-    progressDialog.show();
+    // progressDialog.show();
   }
 
   void hideProgress() {
