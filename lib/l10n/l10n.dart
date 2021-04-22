8c78caf1d39eb9279e946d00121cfcbdb6a44411
Shubham Gupta <gshubham030@gmail.com>
2021-04-22 23:13:41 +0530

    lang update

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -5,7 +5,6 @@
 
 import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
-
 import 'messages_all.dart';
 
 class S {
@@ -362,7 +361,7 @@ class S {
       );
 
   String get bio_description => Intl.message(
-        'Please tell us a little bit about yourself in a few sentences. For example, you can tell us what makes you unique.',
+        'Please tell us a little bit about yourself in a few sentences.For example, you can tell us what makes you unique.',
         name: 'bio_description',
       );
 
