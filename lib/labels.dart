6939ad3ed5e9ff99cb79554c11dbd59a5471c49a
Burhan ud din <burhan@uipep.com>
2021-05-19 09:50:48 +0530

    label fixes

--- lib/labels.dart
+++ lib/labels.dart
@@ -4,12 +4,21 @@ class L {
   L.of(BuildContext context) {}
 
   //OFFERS LABELS STARTS HERE
+// NOTIFICATIONS
+// 
 
   String get invitation_accepted => "Invitation Accepted";
   String get invitation_accepted_subtitle =>
       " has accepted your offer and has shared an invitation.";
+
+  String get offer_invitation_notification_title => "Offer Invitation";
+  String get offer_invitation_notification_subtitle =>
+      " has invited you to accept an offer.";
+
+
+
   String get accept_offer_invitation_confirmation =>
-      "By acceting, a task will be added to your pending tasks.";
+      "By accepting, a task will be added to your pending tasks.";
   String get entered_credits_less_than_minimum_credits =>
       "Entered credits should be greater than minimum credits";
 
@@ -19,9 +28,9 @@ class L {
   String get minimum_credit_title => "Minimum Credits*";
   String get minimum_credit_hint => "Provide minimum credits you require";
 
-  String get offer_invitation_notification_title => "Offer Invitation";
-  String get offer_invitation_notification_subtitle =>
-      " has invited you to accept its offer.";
+
+
+
   String get select_a_speaker => "Select a speaker";
   String get registration_link => "Registration Link";
   String get registration_link_hint => "Ex: Eventbrite link, etc.";
