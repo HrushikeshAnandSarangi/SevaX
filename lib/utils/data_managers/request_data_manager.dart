f1f2f5d8a65ae803b2369a8dcfce17bdbbbec8b9
Burhan ud din <burhan@uipep.com>
2021-06-09 18:25:50 +0530

    Fixes

--- lib/utils/data_managers/request_data_manager.dart
+++ lib/utils/data_managers/request_data_manager.dart
@@ -2182,9 +2182,7 @@ Stream<List<CategoryModel>> getAllCategoriesStream() async* {
       StreamTransformer<QuerySnapshot, List<CategoryModel>>.fromHandlers(
     handleData: (snapshot, sink) {
       List<CategoryModel> categories = [];
-
-      logger.i(snapshot.documents.length.toString() + "List Categrories from ");
-      snapshot.documents.forEach((element) {
+       snapshot.documents.forEach((element) {
         CategoryModel model = CategoryModel.fromMap(element.data);
         model.typeId = element.documentID;
         log('${model.title_en}');
